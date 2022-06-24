99d2e8e4bb7a978956c589462f7ccef7043992b1
FahimBeck <fahimbeck@gmail.com>
2022-06-24 03:58:48 +0200

    Update

--- R/globals.R
+++ R/globals.R
@@ -3,12 +3,12 @@
 
 # Modify data.dir to indicate the location of the GWAStutorial files
 # Intermediate data files will also be stored in this same location unless you set out.dir
-data.dir = paste(getwd(), 'GWAS_Tutorial', sep = "/")
-out.dir <- paste(getwd(), 'GWAS_Tutorial/out', sep = "/")
+data.dir = paste(getwd(), 'Data', sep = "/")
+out.dir <- paste(getwd(), 'Data/out', sep = "/")
 
-gwas.fn <- lapply( c (bed='bed', bim='bim', fam='fam' ,gds='gds'), function (n) sprintf ("%s/GWAStutorial.%s", data.dir, n))
+gwas.fn <- lapply(c(bed='bed', bim='bim', fam='fam' ,gds='gds'), function (n) sprintf ("%s/GWAStutorial.%s", data.dir, n))
 clinical.fn <- sprintf("%s/GWAStutorial_clinical.csv", data.dir)
-onethou.fn = lapply(c(info='info' ,ped='ped'), function(n) sprintf("%s/chr16_1000g_CEU.%s", data.dir, n))
+onethou.fn = lapply(c(info='info', ped='ped'), function(n) sprintf("%s/chr16_1000g_CEU.%s", data.dir, n))
 protein.coding.coords.fname <- sprintf ("%s/ProCodgene_coords.csv", data.dir)
 
 # Output files
